d42c39d8aa65dc7d76f51de58bf7d52f9e8353a0
Praveen R <pwr5109@gmail.com>
2021-05-01 02:24:29 -0700

    Database_Update_1.0

---
+++ Database/SQL Set-up Files/teams_table.sql
@@ -0,0 +1,21 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "teams" (
+    "LEAGUE_ID" int   NOT NULL,
+    "TEAM_ID" int   NOT NULL,
+    "MIN_YEAR" int   NOT NULL,
+    "MAX_YEAR" int   NOT NULL,
+    "ABBREVIATION" varchar   NOT NULL,
+    "NICKNAME" varchar   NOT NULL,
+    "YEARFOUNDED" int   NOT NULL,
+    "CITY" varchar   NOT NULL,
+    "ARENA" varchar   NOT NULL,
+    "ARENACAPACITY" int   NOT NULL,
+    "OWNER" varchar   NOT NULL,
+    "GENERALMANAGER" varchar   NOT NULL,
+    "HEADCOACH" varchar   NOT NULL,
+    "DLEAGUEAFFILIATION" varchar   NOT NULL
+);
+
